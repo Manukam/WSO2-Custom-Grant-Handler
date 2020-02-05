6228da1999437a390d3188bb6eef457b5f4fa946
ManukaH <maduranga.manuka3@gmail.com>
2020-02-05 13:52:32 +0530

    Improvements

--- src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
+++ src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
@@ -1,39 +1,46 @@
 package org.wso2.sample.custom.grant.type;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
-import org.wso2.carbon.identity.base.IdentityConstants;
 import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth.cache.OAuthCache;
 import org.wso2.carbon.identity.oauth.cache.OAuthCacheKey;
-import org.wso2.carbon.identity.oauth.common.exception.InvalidOAuthClientException;
-import org.wso2.carbon.identity.oauth.dao.OAuthAppDAO;
-import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2AccessTokenReqDTO;
 import org.wso2.carbon.identity.oauth2.model.AuthzCodeDO;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
-import org.wso2.carbon.identity.oauth2.token.handlers.grant.AbstractAuthorizationGrantHandler;
+import org.wso2.carbon.identity.oauth2.token.handlers.grant.AuthorizationCodeGrantHandler;
 import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
-import org.wso2.carbon.identity.oauth2.dao.*;
+import org.wso2.carbon.identity.mgt.constants.IdentityMgtConstants;
 
-public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
+import java.io.*;
+import java.util.Properties;
+
+public class CustomGrantHandler extends AuthorizationCodeGrantHandler {
 
     public static final String CLIENT_UUID_PARAM = "uuidClient";
     public static final String AUTHORIZATION_CODE_PARAM = "code";
+    public static Properties properties = new Properties();
     private static Log log = LogFactory.getLog(CustomGrantHandler.class);
 
     @Override
     public boolean validateGrant(OAuthTokenReqMessageContext tokReqMsgCtx) throws IdentityOAuth2Exception {
         super.validateGrant(tokReqMsgCtx);
-        OAuth2AccessTokenReqDTO tokenReq = tokReqMsgCtx.getOauth2AccessTokenReqDTO();
+        if(properties.isEmpty()){
+            readPropertiesFromFile();
+        }
+        String singleDeviceClaim = (properties.getProperty("Single.Device.Claim").trim());
+        if(singleDeviceClaim.isEmpty()){
+            log.info("Single Device Claim Value is not configured. Skipping the validation");
+            return true;
+        }
         RequestParameter[] parameters = tokReqMsgCtx.getOauth2AccessTokenReqDTO().getRequestParameters();
-        String uuidClient = null;
+        String uuidClient = null;  //UUID sent by Client
         String authorizationCode = null;
+        String uuidIS = null; //UUID in IS side
         // find out Uuid Parameter
         for (RequestParameter parameter : parameters) {
             if (CLIENT_UUID_PARAM.equals(parameter.getKey())) {
@@ -42,38 +49,29 @@ public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
                 }
             } else if (AUTHORIZATION_CODE_PARAM.equals(parameter.getKey())) {
                 authorizationCode = parameter.getValue()[0];
-
             }
         }
 
-        tokenReq.setAuthorizationCode(authorizationCode);
+        tokReqMsgCtx.getOauth2AccessTokenReqDTO().setAuthorizationCode(authorizationCode);
+        OAuth2AccessTokenReqDTO tokenReq = tokReqMsgCtx.getOauth2AccessTokenReqDTO();
         AuthzCodeDO authzCodeBean = getPersistedAuthzCode(tokenReq);
-
-        OAuthAppDO appInfo;
-        OAuthAppDAO oAuthAppDAO = new OAuthAppDAO();
-        String uuidIS = null;
         AuthenticatedUser user = authzCodeBean.getAuthorizedUser();
-        try {
-            appInfo = oAuthAppDAO.getAppInformation(tokReqMsgCtx.getOauth2AccessTokenReqDTO().getClientId());
-//            if (appInfo.getApplicationName().equals("Playground_SP")) {    // Name of the Application
+        tokReqMsgCtx.setAuthorizedUser(user);
+        tokReqMsgCtx.setScope(tokReqMsgCtx.getOauth2AccessTokenReqDTO().getScope());
 
-                uuidIS = CarbonContext.getThreadLocalCarbonContext().getUserRealm().getUserStoreManager()
-                        .getUserClaimValue(user.getUserName(), "http://wso2.org/claims/organization", null);
-//            }
-        } catch (InvalidOAuthClientException e) {
-            log.error(e);
+        try {
+            uuidIS = CarbonContext.getThreadLocalCarbonContext().getUserRealm().getUserStoreManager()
+                    .getUserClaimValue(user.getUserName(), singleDeviceClaim, null);
         } catch (org.wso2.carbon.user.api.UserStoreException e) {
             e.printStackTrace();
         }
 
-
-        //if(request.getParameter("uuidClient").isEmpty()){ //Checking if UUID sent by the user is null
         if (uuidClient != null) {//Checking if UUID sent by the user is null
             if (uuidIS != null) {
                 if (uuidIS.equals(uuidClient)) {
                     return true;   //valid user from same device
                 } else {
-                    return false; //new device invalid user
+                    throw new IdentityOAuth2Exception("Invalid User");//new device invalid user
                 }
             } else {
                 //Invalid Request
@@ -86,7 +84,7 @@ public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
 
     }
 
-    private AuthzCodeDO getPersistedAuthzCode(OAuth2AccessTokenReqDTO tokenReqDTO) throws IdentityOAuth2Exception {
+    private AuthzCodeDO getPersistedAuthzCode(OAuth2AccessTokenReqDTO tokenReqDTO) {
 
         AuthzCodeDO authzCodeDO;
         // If cache is enabled, check in the cache first.
@@ -107,34 +105,29 @@ public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
             log.debug("Retrieving authorization code information from db for client id : " + tokenReqDTO.getClientId());
         }
 
-        AuthorizationCodeValidationResult validationResult = OAuthTokenPersistenceFactory.getInstance()
-                .getAuthorizationCodeDAO().validateAuthorizationCode(tokenReqDTO.getClientId(),
-                        tokenReqDTO.getAuthorizationCode());
-        if (validationResult != null) {
-            if (!validationResult.isActiveCode()) {
-                //revoking access token issued for authorization code as per RFC 6749 Section 4.1.2
-                revokeExistingAccessTokens(validationResult.getTokenId(), validationResult.getAuthzCodeDO());
-            }
-            return validationResult.getAuthzCodeDO();
-        } else {
-            // This means an invalid authorization code was sent for validation. We return null since higher
-            // layers expect a null value for an invalid authorization code.
-            return null;
-        }
+        return null;
     }
 
-    private void revokeExistingAccessTokens(String tokenId, AuthzCodeDO authzCodeDO) throws IdentityOAuth2Exception {
-        OAuthTokenPersistenceFactory.getInstance().getAccessTokenDAO().revokeAccessToken(tokenId, authzCodeDO
-                .getAuthorizedUser().toString());
-
-        if (log.isDebugEnabled()) {
-            if (IdentityUtil.isTokenLoggable(IdentityConstants.IdentityTokens.AUTHORIZATION_CODE)) {
-                log.debug("Validated authorization code(hashed): " + DigestUtils.sha256Hex(authzCodeDO
-                        .getAuthorizationCode()) + " for client: " + authzCodeDO.getConsumerKey() + " is not active. " +
-                        "So revoking the access tokens issued for the authorization code.");
-            } else {
-                log.debug("Validated authorization code for client: " + authzCodeDO.getConsumerKey() + " is not " +
-                        "active. So revoking the access tokens issued for the authorization code.");
+    private static void readPropertiesFromFile() {
+        InputStream inStream = null;
+        File pipConfigXml = new File(IdentityUtil.getIdentityConfigDirPath(), IdentityMgtConstants.PropertyConfig
+                .CONFIG_FILE_NAME);
+        if (pipConfigXml.exists()) {
+            try {
+                inStream = new FileInputStream(pipConfigXml);
+                properties.load(inStream);
+            } catch (FileNotFoundException e) {
+                log.error("Can not load identity-mgt properties file ", e);
+            } catch (IOException e) {
+                log.error("Can not load identity-mgt properties file ", e);
+            } finally {
+                if (inStream != null) {
+                    try {
+                        inStream.close();
+                    } catch (IOException e) {
+                        log.error("Error while closing stream ", e);
+                    }
+                }
             }
         }
     }
