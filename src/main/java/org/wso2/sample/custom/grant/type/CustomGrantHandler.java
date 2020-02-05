9c1a2d6017e485e9fde8a806f06bb47c4e3d548c
Manuka Maduranga <maduranga.manuka3@gmail.com>
2020-02-05 11:20:45 +0530

    Update CustomGrantHandler.java

--- src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
+++ src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
@@ -1,12 +1,9 @@
 package org.wso2.sample.custom.grant.type;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
-import org.wso2.carbon.identity.base.IdentityConstants;
-import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth.cache.OAuthCache;
 import org.wso2.carbon.identity.oauth.cache.OAuthCacheKey;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
@@ -14,11 +11,10 @@ import org.wso2.carbon.identity.oauth2.dto.OAuth2AccessTokenReqDTO;
 import org.wso2.carbon.identity.oauth2.model.AuthzCodeDO;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
-import org.wso2.carbon.identity.oauth2.token.handlers.grant.AbstractAuthorizationGrantHandler;
+import org.wso2.carbon.identity.oauth2.token.handlers.grant.AuthorizationCodeGrantHandler;
 import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
-import org.wso2.carbon.identity.oauth2.dao.*;
 
-public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
+public class CustomGrantHandler extends AuthorizationCodeGrantHandler {
 
     public static final String CLIENT_UUID_PARAM = "uuidClient";
     public static final String AUTHORIZATION_CODE_PARAM = "code";
@@ -97,35 +93,6 @@ public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
             log.debug("Retrieving authorization code information from db for client id : " + tokenReqDTO.getClientId());
         }
 
-        AuthorizationCodeValidationResult validationResult = OAuthTokenPersistenceFactory.getInstance()
-                .getAuthorizationCodeDAO().validateAuthorizationCode(tokenReqDTO.getClientId(),
-                        tokenReqDTO.getAuthorizationCode());
-        if (validationResult != null) {
-            if (!validationResult.isActiveCode()) {
-                //revoking access token issued for authorization code as per RFC 6749 Section 4.1.2
-                revokeExistingAccessTokens(validationResult.getTokenId(), validationResult.getAuthzCodeDO());
-            }
-            return validationResult.getAuthzCodeDO();
-        } else {
-            // This means an invalid authorization code was sent for validation. We return null since higher
-            // layers expect a null value for an invalid authorization code.
-            return null;
-        }
-    }
-
-    private void revokeExistingAccessTokens(String tokenId, AuthzCodeDO authzCodeDO) throws IdentityOAuth2Exception {
-        OAuthTokenPersistenceFactory.getInstance().getAccessTokenDAO().revokeAccessToken(tokenId, authzCodeDO
-                .getAuthorizedUser().toString());
-
-        if (log.isDebugEnabled()) {
-            if (IdentityUtil.isTokenLoggable(IdentityConstants.IdentityTokens.AUTHORIZATION_CODE)) {
-                log.debug("Validated authorization code(hashed): " + DigestUtils.sha256Hex(authzCodeDO
-                        .getAuthorizationCode()) + " for client: " + authzCodeDO.getConsumerKey() + " is not active. " +
-                        "So revoking the access tokens issued for the authorization code.");
-            } else {
-                log.debug("Validated authorization code for client: " + authzCodeDO.getConsumerKey() + " is not " +
-                        "active. So revoking the access tokens issued for the authorization code.");
-            }
-        }
+        return null;
     }
 }
