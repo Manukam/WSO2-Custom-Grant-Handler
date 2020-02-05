baf3cc7b805bbc8a363a444d3c44ae64fe0c2937
ManukaH <maduranga.manuka3@gmail.com>
2020-02-05 20:57:18 +0530

    Improvements

--- src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
+++ src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
@@ -3,23 +3,17 @@ package org.wso2.sample.custom.grant.type;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.CarbonContext;
-import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.core.util.IdentityUtil;
-import org.wso2.carbon.identity.oauth.cache.OAuthCache;
-import org.wso2.carbon.identity.oauth.cache.OAuthCacheKey;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
-import org.wso2.carbon.identity.oauth2.dto.OAuth2AccessTokenReqDTO;
-import org.wso2.carbon.identity.oauth2.model.AuthzCodeDO;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
-import org.wso2.carbon.identity.oauth2.token.handlers.grant.AbstractAuthorizationGrantHandler;
-import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
+import org.wso2.carbon.identity.oauth2.token.handlers.grant.AuthorizationCodeGrantHandler;
 import org.wso2.carbon.identity.mgt.constants.IdentityMgtConstants;
 
 import java.io.*;
 import java.util.Properties;
 
-public class CustomGrantHandler extends AbstractAuthorizationGrantHandler {
+public class CustomGrantHandler extends AuthorizationCodeGrantHandler {
 
     public static final String CLIENT_UUID_PARAM = "uuidClient";
     public static Properties properties = new Properties();
