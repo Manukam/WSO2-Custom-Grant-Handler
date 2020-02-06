55ce7f015a97c0657761500e237f8507b4cd00a1
ManukaH <maduranga.manuka3@gmail.com>
2020-02-06 14:22:52 +0530

    Improvements

--- src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
+++ src/main/java/org/wso2/sample/custom/grant/type/CustomGrantHandler.java
@@ -1,21 +1,27 @@
 package org.wso2.sample.custom.grant.type;
 
+import org.apache.axiom.om.OMElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.CarbonContext;
-import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
-import org.wso2.carbon.identity.mgt.constants.IdentityMgtConstants;
 import org.wso2.carbon.identity.oauth2.token.handlers.grant.AuthorizationCodeGrantHandler;
+import org.wso2.carbon.identity.core.util.IdentityConfigParser;
 
-import java.io.*;
+import javax.xml.namespace.QName;
+import java.util.Iterator;
 import java.util.Properties;
 
 public class CustomGrantHandler extends AuthorizationCodeGrantHandler {
 
-    public static final String CLIENT_UUID_PARAM = "uuidClient";
+    public static final String CLAIM_URI = "claimURI";
+    public static final String CLIENT_PARAMETER = "clientParameter";
+    public static final String SINGLE_DEVICE_CONFIG = "SingleDeviceConfigs";
+    public static final String CONFIG_ELEM_OAUTH = "OAuth";
+    public static String CLIENT_UUID_PARAM;
+    public static String USER_CLAIM;
     private static final String AUTHORIZATION_CODE_PARAM = "code";
     private static Properties properties = new Properties();
     private static Log log = LogFactory.getLog(CustomGrantHandler.class);
@@ -32,21 +38,17 @@ public class CustomGrantHandler extends AuthorizationCodeGrantHandler {
                 uuidClient = parameter.getValue()[0];
             }
         }
-        tokReqMsgCtx.getOauth2AccessTokenReqDTO().setAuthorizationCode(tokReqMsgCtx.getOauth2AccessTokenReqDTO().getRequestParameters()[0].getValue()[0].toString());
+//        tokReqMsgCtx.getOauth2AccessTokenReqDTO().setAuthorizationCode(tokReqMsgCtx.getOauth2AccessTokenReqDTO().getRequestParameters()[0].getValue()[0].toString());
         tokReqMsgCtx.setScope(tokReqMsgCtx.getOauth2AccessTokenReqDTO().getScope());
         super.validateGrant(tokReqMsgCtx);
-        if (properties.isEmpty()) {
+
+        if (CLIENT_UUID_PARAM == null || USER_CLAIM == null) {
             readPropertiesFromFile();
-        }
-        String singleDeviceClaim = (properties.getProperty("Single.Device.Claim").trim());
-        if (singleDeviceClaim.isEmpty()) {
-            log.info("Single Device Claim Value is not configured. Skipping the validation");
-            return true;
         }
 
         try {
             uuidIS = CarbonContext.getThreadLocalCarbonContext().getUserRealm().getUserStoreManager()
-                    .getUserClaimValue(tokReqMsgCtx.getAuthorizedUser().getUserName(), singleDeviceClaim, null);
+                    .getUserClaimValue(tokReqMsgCtx.getAuthorizedUser().getUserName(), USER_CLAIM, null);
         } catch (org.wso2.carbon.user.api.UserStoreException e) {
             if (log.isDebugEnabled()) {
                 log.debug("User Store Exception", e);
@@ -72,25 +74,29 @@ public class CustomGrantHandler extends AuthorizationCodeGrantHandler {
 
     }
 
-    private static void readPropertiesFromFile() {
-        InputStream inStream = null;
-        File pipConfigXml = new File(IdentityUtil.getIdentityConfigDirPath(), IdentityMgtConstants.PropertyConfig
-                .CONFIG_FILE_NAME);
-        if (pipConfigXml.exists()) {
-            try {
-                inStream = new FileInputStream(pipConfigXml);
-                properties.load(inStream);
-            } catch (FileNotFoundException e) {
-                log.error("Can not load identity-mgt properties file ", e);
-            } catch (IOException e) {
-                log.error("Can not load identity-mgt properties file ", e);
-            } finally {
-                if (inStream != null) {
-                    try {
-                        inStream.close();
-                    } catch (IOException e) {
-                        log.error("Error while closing stream ", e);
-                    }
+    private void readPropertiesFromFile() {
+        IdentityConfigParser configParser = IdentityConfigParser.getInstance();
+        OMElement oauthElem = configParser.getConfigElement(CONFIG_ELEM_OAUTH);
+
+        if (oauthElem == null) {
+            log.error("OAuth element is not available.");
+            return;
+        }
+        parseSingleDeviceConfig(oauthElem);
+    }
+
+    private void parseSingleDeviceConfig(OMElement singleDeviceConfig) {
+        if (singleDeviceConfig == null) {
+            return;
+        }
+
+        Iterator validators = singleDeviceConfig.getChildrenWithLocalName(SINGLE_DEVICE_CONFIG);
+        if (validators != null) {
+            for (; validators.hasNext(); ) {
+                OMElement validator = (OMElement) validators.next();
+                if (validator != null) {
+                    CLIENT_UUID_PARAM = validator.getAttributeValue(new QName(CLIENT_PARAMETER));
+                    USER_CLAIM = validator.getAttributeValue(new QName(CLAIM_URI));
                 }
             }
         }
